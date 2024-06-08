e3506acf8a35ce54379a9f3ff76822afb42c31b2
Shreyash Galgale <shreyasgalgale019@gmail.com>
2024-06-08 14:21:36 +0530

    svg changed with image and checkbox

---
+++ script.js
@@ -0,0 +1,13 @@
+const TODO_DIV_CLASSES = "todo flex flex-row justify-between px-3 m-3 w-auto ";
+const TASK_DIV_CLASSES =
+    "task flex flex-row space-x-3 justify-center items-center align-middle";
+
+
+let submitBtn = document.querySelector("#submit");
+submitBtn.addEventListener("click", (e) => {
+    let taskElement = createElement();
+});
+
+function createElement() {
+    const taskDiv = document.createElement("div");
+}
