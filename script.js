7f86bdef286c026a0224ed73d9586711cfe3f2a5
Shreyash Galgale <shreyasgalgale019@gmail.com>
2024-06-25 13:36:06 +0530

    localstorage, with checked unchecked problem

--- script.js
+++ script.js
@@ -3,16 +3,37 @@ const HR_TEMPLATE = document.querySelector("#hr-break");
 const taskList = document.querySelector(".list");
 const taskInput = document.querySelector("#task-input");
 
+document.addEventListener("DOMContentLoaded", () => {
+    const todoTitles = JSON.parse(localStorage.getItem("todoTitles")) || [];
+    if (todoTitles) {
+        todoTitles.forEach((title) => {
+            addTask(title);
+        });
+    }
+});
+
 let submitBtn = document.querySelector("#submit-btn");
-submitBtn.addEventListener("click", addTask);
+submitBtn.addEventListener("click", () => {
+    let taskStr = taskInput.value;
+    addTask(taskStr);
+    const todoTitles = JSON.parse(localStorage.getItem("todoTitles")) || [];
+    todoTitles.push(taskStr);
+    localStorage.setItem("todoTitles", JSON.stringify(todoTitles));
+});
 taskInput.addEventListener("keypress", (e) => {
-    if (e.keyCode == 13) addTask();
+    if (e.keyCode == 13) {
+        let taskStr = e.target.value;
+        addTask(taskStr);
+        const todoTitles = JSON.parse(localStorage.getItem("todoTitles")) || [];
+        todoTitles.push(taskStr);
+        localStorage.setItem("todoTitles", JSON.stringify(todoTitles));
+    }
 });
 function getRandomId() {
     return Math.random().toString(36).substr(2, 9);
 }
 
-function addTask() {
+function addTask(taskStr, isComplete = false) {
     let newTask = TASK_TEMPLATE.cloneNode(true);
     let newHr = HR_TEMPLATE.cloneNode(true);
 
@@ -20,7 +41,6 @@ function addTask() {
     newHr.classList.remove("hidden");
     newTask.setAttribute("id", getRandomId());
     newHr.setAttribute("id", getRandomId());
-    let taskStr = taskInput.value;
     newTask.querySelector("img").addEventListener("click", () => {
         deleteTask(newTask);
     });
@@ -55,6 +75,7 @@ function addTask() {
 }
 
 function deleteTask(newTask) {
+    const todoTitleToRemove = newTask.querySelector("h1").innerText;
     if (newTask.nextSibling) {
         newTask.nextSibling.remove();
         newTask.remove();
@@ -64,4 +85,7 @@ function deleteTask(newTask) {
     } else {
         newTask.remove();
     }
+    let todoTitles = JSON.parse(localStorage.getItem("todoTitles")) || [];
+    todoTitles = todoTitles.filter((title) => title !== todoTitleToRemove);
+    localStorage.setItem("todoTitles", JSON.stringify(todoTitles));
 }
